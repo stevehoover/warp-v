bb929afb2a723e9ad56b4e592ba8e8edcc6bf2d0
Steve Hoover <steve_git@hooverfam.net>
2018-09-24 16:47:16 -0400

    Restrucured repo to rename 'warpv_formal' to 'formal' and include riscv-formal repo as a submodule of 'formal'. Cleaned up Makefile and READMEs.

--- formal/make_env.sh
+++ formal/make_env.sh
@@ -113,7 +113,7 @@ if [ $? -eq 1 ]; then
 fi
 
 cd "$BUILD_DIR"
-if (( ${STATUS[yosys]} || $STATUS[SymbiYosys] || $STATUS[boolector] )); then
+if (( ${STATUS[yosys]} || ${STATUS[SymbiYosys]} || ${STATUS[boolector]} )); then
   echo && \
   echo "*********************" && \
   echo "Some build(s) FAILED." && \
