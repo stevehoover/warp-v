b2e6692b18b1d548eaede26d175a508621dfd07e
Steve Hoover <steve_git@hooverfam.net>
2021-07-27 18:10:13 -0400

    Updating warp-v version for configurator.

--- configurator/src/utils/WarpVUtils.js
+++ configurator/src/utils/WarpVUtils.js
@@ -2,5 +2,5 @@ export function getWarpVFileForCommit(version) {
     return `https://raw.githubusercontent.com/stevehoover/warp-v/${version}/warp-v.tlv`
 }
 
-export const warpVLatestSupportedCommit = "bfef855a246e9ebb2a37147c1ae772ac65a1e244"
+export const warpVLatestSupportedCommit = "3164c2a5ce0e9eeb247de1e1d0fe939aa07549bf"
 export const warpVLatestVersionCommit = "master"
